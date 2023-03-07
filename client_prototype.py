22f2a5805cffa8e7dfd9d7ffa0686b9a0bbfba71
UnslaadTovitaan <UnslaadTovitaan@gmail.com>
2023-03-07 22:26:57 +0100

    Prototype avancé avec un bureaux et un shell

--- client_prototype.py
+++ client_prototype.py
@@ -5,12 +5,101 @@ pygame.init()
 
 # constantes
 
-WINDOW = pygame.display.set_mode((800, 640), pygame.RESIZABLE)
+WINDOW = pygame.display.set_mode((1080, 640), pygame.RESIZABLE)
 POLICE = pygame.font.Font(
     'ressources/fonts/sourcecodepro.ttf', 16)  # font à changer
 POLICE.bold = True
 
-# fonctions de maniment de texte
+# classes de gameplay
+
+
+class Application:
+    current_app: 'Application'
+
+    def __init__(self, icon_nom: str, event_liste: List[int]) -> None:
+        self.event_liste = event_liste
+        self.icon: pygame.Surface = pygame.image.load(icon_nom)
+
+    def get_focus(self):
+        """focus dans l'application"""
+        Application.current_app = self
+
+    def handle_event(self, event: pygame.event.Event) -> None:
+        """met à jour correctement le texte selon l'entrée de l'utilisateur"""
+        raise NotImplementedError
+
+    def update(self) -> None:
+        """mise à jour"""
+        raise NotImplementedError
+    
+
+class Link:
+    """lien vers une application"""
+    def __init__(self, app: Application, icon_nom: str, pos: Tuple[int, int], scale: float=1) -> None:
+        """constructeur"""
+        self.icon = pygame.transform.scale_by(pygame.image.load(icon_nom), scale)
+        self.rect = self.icon.get_rect()
+        self.pos = pos
+        self.app = app
+
+    def onclick(self):
+        """action lors du click"""
+        self.app.get_focus()
+
+    def render(self):
+        return self.icon, self.pos
+
+
+class Shell(Application):
+    """gestion des shells"""
+    def __init__(self, icon_nom: str, event_liste: List[int]) -> None:
+        super().__init__(icon_nom, event_liste)
+
+    def handle_event(self, event: pygame.event.Event):
+        """met à jour correctement le texte selon l'entrée de l'utilisateur"""
+        match event.key:
+            case pygame.K_ESCAPE:
+                bureaux.get_focus()
+            case pygame.K_LEFT:
+                texte.recule_curseur()
+            case pygame.K_RIGHT:
+                texte.avance_curseur()
+            case pygame.K_BACKSPACE:
+                texte.sup_at()
+            case _:
+                lettre = event.unicode
+                texte.insert_at(lettre)
+
+    def update(self):
+        """mise à jour"""
+        texte_surfaces = render_texte(str(texte), 100)
+        blit_sequence: Any = texte_wrapper(texte_surfaces, (100, 100))
+        WINDOW.blits(blit_sequence)
+        WINDOW.blit(*render_curseur(texte_surfaces, (100, 100), 100))
+
+
+class Desktop(Application):
+    """gestion des bureaux"""
+    def __init__(self, background_nom: str, icon_nom: str, apps: List[Link], event_liste: List[int]) -> None:
+        super().__init__(icon_nom, event_liste)
+        self.background = pygame.transform.scale(pygame.image.load(background_nom), WINDOW.get_size())
+
+        self.apps = apps
+
+    def handle_event(self, event: pygame.event.Event) -> None:
+        """gère les événements"""
+        for link in self.apps:
+            pos = (event.pos[0] - link.pos[0], event.pos[1] - link.pos[1])
+            if link.rect.collidepoint(pos):
+                link.onclick()
+    
+    def update(self) -> None:
+        """met à jour"""
+        WINDOW.blit(self.background, (0, 0))
+        WINDOW.blits([link.render() for link in self.apps])
+
+
+# fonctions et classes de maniment du texte
 
 
 class Texte:
@@ -20,12 +109,16 @@ class Texte:
 
     def avance_curseur(self):
         """avance le curseur"""
-        self.curseur = self.curseur + \
-            1 if len(self.texte) > self.curseur else self.curseur
+        self.curseur, success = ((self.curseur + 1, True) if
+                                 len(self.texte) > self.curseur
+                                 else (self.curseur, False))
+        return success
 
     def recule_curseur(self):
         """avance le curseur"""
-        self.curseur = self.curseur - 1 if self.curseur > 0 else self.curseur
+        self.curseur, success = ((self.curseur - 1, True) if
+                                 self.curseur > 0 else (self.curseur, False))
+        return success
 
     def insert_at(self, char: str):
         """avance le curseur"""
@@ -34,38 +127,18 @@ class Texte:
 
     def sup_at(self):
         """supprime le caractère à la position du curseur"""
-        self.recule_curseur()
-        self.texte.pop(self.curseur)
+        success = self.recule_curseur()
+        if success: self.texte.pop(self.curseur)
 
     def __str__(self) -> str:
         return "".join(self.texte)
 
 
-def update_texte(event: pygame.event.Event):
-    """met à jour correctement le texte selon l'entrée de l'utilisateur"""
-    match event.key:
-        case pygame.K_ESCAPE:
-            ...
-        case pygame.K_LEFT:
-            texte.recule_curseur()
-        case pygame.K_RIGHT:
-            texte.avance_curseur()
-        case pygame.K_BACKSPACE:
-            texte.sup_at()
-        case _:
-            lettre = event.unicode
-            texte.insert_at(lettre)
-
-
 # fonctions de surface
 
-
-def update():
-    """fonction de mise à jour"""
-    texte_surfaces = render_texte(str(texte), 100)
-    blit_sequence: Any = texte_wrapper(texte_surfaces, (100, 100))
-    WINDOW.blits(blit_sequence)
-    WINDOW.blit(*render_curseur(texte_surfaces, (100, 100), 100))
+def clear():
+    """nettoie l'écran"""
+    WINDOW.fill("#000000")
 
 
 def texte_wrapper(texte_surfaces: List[pygame.Surface], pos: Tuple[float, float]):
@@ -94,7 +167,8 @@ def render_curseur(texte_surfaces: List[pygame.Surface], pos: Tuple[int, int], m
     surf = pygame.Surface((1, texte_surfaces[0].get_height()))
     surf.fill("#FFFFFF")
     size = POLICE.size(str(texte)[:texte.curseur])
-    position = (pos[0] + size[0] % max_length, pos[1] + (size[0] // max_length) * size[1])
+    position = (pos[0] + size[0] % (max_length + 1), pos[1] +
+                (size[0] // (max_length + 1)) * size[1])
     return surf, position
 
 
@@ -104,25 +178,32 @@ def main():
     """boucle principale"""
     running = True
 
+    Application.current_app = shell
+    bureaux.get_focus()
+
     while running:
+        clear()
         events = pygame.event.get()
         for evnt in events:
             match evnt.type:
                 case pygame.QUIT:
                     running = False
-                case pygame.KEYDOWN:
-                    update_texte(evnt)
                 case _:
                     ...
+            # on passe l'événement dans l'application
+            if evnt.type in Application.current_app.event_liste:
+                Application.current_app.handle_event(evnt)
 
-        update()
+        Application.current_app.update()
 
         pygame.display.flip()
 
 # variables
 
-
 texte = Texte()
+shell = Shell('ressources/icons/cmd.png', [pygame.KEYDOWN])
+bureaux = Desktop('ressources/background/desktop.jpg', 'ressources/icons/bureaux.png',
+                  [Link(shell, 'ressources/icons/cmd.png', (100, 100), 0.5)], [pygame.MOUSEBUTTONDOWN])
 
 # appel de la fonction principale
 
