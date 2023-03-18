62f96827ba7d0a4d642fa7ca7e3633a6f4d64c64
UnslaadTovitaan <UnslaadTovitaan@gmail.com>
2023-03-18 18:53:12 +0100

    commande déroulante

--- modules/procesus.py
+++ modules/procesus.py
@@ -18,6 +18,7 @@ class Commande:
         """renvoie la commande correspondant au nom"""
         return Commande.commandes_nom_dct[nom]
 
+
 class Interpreter:
     def __init__(self, texte: 'Texte') -> None:
         self.texte = texte
@@ -27,7 +28,7 @@ class Interpreter:
         # cas de commande vide
         if commande == "":
             return
-        
+
         # on commence par passer à la ligne suivante
         self.texte.newline()
 
@@ -48,21 +49,30 @@ class TextRenderer:
         self.pos = pos
         self.texte = texte
 
+        self.nb_line = 20
+
     def texte_wrapper(self, texte_surfaces: List[pygame.Surface]):
         """retourne le texte à la ligne"""
-        height = texte_surfaces[0].get_height()
+        height = 21
         return ((surf, (self.pos[0], self.pos[1] + k * height)) for k, surf in enumerate(texte_surfaces))
 
     def split_surface(self, surface: pygame.Surface):
         """découpe une longue surface selon le paramètre max_length"""
-        liste_surface: List[pygame.Surface] = [pygame.Surface((self.texte.max_length, surface.get_height()))
-                                               for _ in range(surface.get_width() // self.texte.max_length + 1)]
+        liste_surface: List[pygame.Surface] = [pygame.Surface((self.texte.max_length, 21))
+                                               for _ in range(self.nb_line)]
 
+        # la première surface contient le texte le plus ancient
         for k, surf in enumerate(liste_surface):
-            surf.blit(surface, (-k * self.texte.max_length, 0))
+            
+            if (-((surface.get_width() // self.texte.max_length) * self.texte.max_length)
+                                + (self.nb_line) * self.texte.max_length) <= 0:
+                surf.blit(surface, (-((surface.get_width() // self.texte.max_length) * self.texte.max_length)
+                                    + (self.nb_line - k - 1) * self.texte.max_length, 0))
+            else:
+                surf.blit(surface, (-k * self.texte.max_length, 0))
         return liste_surface
 
-    def render_texte(self, texte: str, max_length: int):
+    def render_texte(self):
         """affiche le texte"""
         return self.split_surface(self.texte.police.render(str(self.texte), True, "#30FF00"))
 
@@ -73,7 +83,7 @@ class TextRenderer:
         size = self.texte.police.size(
             str(self.texte)[:self.texte.curseur + len(self.texte.pre_texte)])
         position = (self.pos[0] + size[0] % (self.texte.max_length), self.pos[1] +
-                    (size[0] // (self.texte.max_length)) * size[1])
+                    min((size[0] // (self.texte.max_length)), self.nb_line - 1) * size[1])
         return surf, position
 
 
@@ -119,7 +129,13 @@ class Texte:
     def register(self):
         """enregistre le texte courant dans pre_texte"""
         self.pre_texte += self.texte
+        size_texte = len(self.texte)
+        size_pretexte = len(self.pre_texte)
         self.vide()
+
+        # on vide une partie du texte au bout d'un moment
+        if size_pretexte > 20 * self.max_length:
+            self.pre_texte = self.pre_texte[size_texte:]
 
     def newline(self):
         """crée une nouvelle ligne"""
@@ -128,7 +144,8 @@ class Texte:
         pos_y = init_pos_y
         while pos_y - init_pos_y == 0:
             self.insert_at(" ")
-            size = self.police.size(str(self)[:self.curseur + len(self.pre_texte)])
+            size = self.police.size(
+                str(self)[:self.curseur + len(self.pre_texte)])
             pos_y: int = (size[0] // (self.max_length))
 
     def __str__(self) -> str:
@@ -141,7 +158,6 @@ class Texte:
 # constante
 
 COMMANDES = [Commande('test', lambda: print('test1')), Commande('scan', lambda: print('test2')),
-             Commande('force', lambda: print('test3')), Commande('check', lambda: print('test4')),
+             Commande('force', lambda: print('test3')), Commande(
+                 'check', lambda: print('test4')),
              Commande('connect', lambda: print('test5'))]
-
-
