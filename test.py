09955179c9c4ecc8debeb6c76270d93f1591ee14
UnslaadTovitaan <111511038+UnslaadTovitaan@users.noreply.github.com>
2023-05-09 13:20:33 +0200

    Update test.py

--- test.py
+++ test.py
@@ -4,16 +4,16 @@ import pygame
 pygame.init()
 
 def depy(t: float):
-    return min((200 * t**2 - 400*t + 500), 500)
+    return min((200 * (t % 4) **2 - 400* (t % 4) + 500), 500)
 
 def depx(t: float):
-    return (40 * t if t < 3 else 120) + 50
+    return (80 * (t % 4) + t // 4 * 160 if t % 4 < 2 else (t // 4 + 1) * 160) + 50
 
 def rot(t: float):
-    return -120 * t if t < 3 else 0
+    return -180 * t if t % 4 < 2 else 0
 
 def appel(fnct: Callable[[float], float], t: float):
-    print(t / 1000)
+    print(t)
     return fnct(t / 1000)
 
 WINDOW = pygame.display.set_mode((600, 600))
@@ -22,21 +22,21 @@ backup = WINDOW.copy()
 carre = pygame.Surface((30, 30), pygame.SRCALPHA)
 carre.fill('#FFFFFF')
 
-
-
-time = pygame.time.get_ticks()
-start_time = time
+clock = pygame.time.Clock()
+time = 0
 
 while True:
     # clear
     WINDOW.blit(backup, (0, 0))
-
-    time = pygame.time.get_ticks() - start_time
 
     image = pygame.transform.rotate(carre, appel(rot, time))
     rect = image.get_rect()
     rect.center = round(appel(depx, time)), round(appel(depy, time))
 
     WINDOW.blit(image, rect)
+
+    dt = clock.tick(60)
+
+    time += dt
 
     pygame.display.flip()
